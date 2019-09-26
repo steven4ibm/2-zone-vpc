9b06397b40cf1907f93ea81cf2583cb82303bf36
hkantare@in.ibm.com <hkantare@in.ibm.com>
2019-09-26 14:05:19 +0530

    Remove variables apikeys

--- provider.tf
+++ provider.tf
@@ -1,8 +1,3 @@
-
-variable "ibmcloud_api_key" {
-  description = "The [IBM Cloud API key](https://cloud.ibm.com/docs/iam?topic=iam-userapikey) with the appropriate permissions to Schematics and VPC infrastructure."
-}
-
 variable "region" {
   description = "The region to create your two VPCs in, such as `us-south`. The VPCs are created in two separate zones within the same region. To get a list of all regions, run `ibmcloud is regions`."
   default = "us-south"
@@ -10,7 +5,6 @@ variable "region" {
 
 
 provider "ibm" {
-  ibmcloud_api_key = "${var.ibmcloud_api_key}"
   ibmcloud_timeout = 300
   generation = "1"
 }

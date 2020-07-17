62127aefec8ca2e47af39a337724fbe1e6497fc0
steven4ibm <steven.eychenne@fr.ibm.com>
2020-07-17 14:32:26 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,4 @@
-variable "region" {
-  description = "The region to create your two VPCs in, such as `eu-de`. To get a list of all regions, run `ibmcloud is regions`."
-  default = "eu-de"
-}
-
-
 provider "ibm" {
   ibmcloud_timeout = 300
-  generation = "1"
+  generation = "2"
 }
